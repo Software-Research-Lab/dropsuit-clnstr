a86e0bd56de342052df3209f5a6b43002841e015
Lado Oniani <ladooniani@gmail.com>
2023-02-18 05:04:47 +0400

    DropSuit

---
+++ infodt.js
@@ -0,0 +1,45 @@
+//#region Dysplay info data
+
+function displayInfoData() {
+  const liblink =
+    "DropSuit: https://github.com/ladooniani/DropSuit#readme\n" +
+    "Copyright © 2016-" +
+    getYear() +
+    " Lado Oniani - DropSuit. All Rights Reserved.\n\n";
+  const libName = "DropSuit NLP module library function:\n";
+  const line =
+    "\n———————————————————————————————————————————————————————————\n\n";
+  const divider =
+    "\n-----------------------------------------------------------\n";
+  const libraryInformation = line + liblink + libName;
+  const functionDescription = `${libraryInformation}
+clnstr(null/string)
+Input: 'myInputString' raw string, or null (to process the constructor input)
+
+Output:
+txt(): Trim, remove punctuation marks and numbers, convert to lower case.
+nmb(): Trim, remove punctuation marks, convert to lower case.
+pnc(): Trim, convert to lower case, remove extra punctuation marks.
+${divider}`;
+
+  const displayInfoData = {
+    descript: functionDescription,
+    line: line,
+  };
+
+  return displayInfoData;
+}
+
+function getYear() {
+  return new Date().getFullYear();
+}
+
+//#endregion
+
+//#region Modules
+
+module.exports = {
+  displayInfoData,
+};
+
+//#endregion
---
+++ test/index.test.js
@@ -0,0 +1,42 @@
+//#region clnstr test
+
+const assert = require("assert");
+const dropsuit_clnstr = require("../index.js");
+
+let input =
+  "  ,.  ,!- ,hello,!bot  ,  ,, so  ,How long           DOes,,&&&swift  - +-   -delivery   service,,,,,,----TYPE:: ,.:/-- :-class     8457'' take?? ? ?,?thank  you !??!,?!.. . .!..  ...  ";
+let expected_output_clnstr_txt =
+  "hello bot so how long does swift delivery service type class take thank you";
+let expected_output_clnstr_nmb =
+  "hello bot so how long does swift delivery service type class 8457 take thank you";
+let expected_output_clnstr_pnc =
+  "hello, bot, so, how long does, swift - delivery service, type: class 8457' take? thank you!";
+
+console.log("\n\nInput:", [input]);
+
+describe("dropsuit-clnstr", () => {
+  describe("clnstr()", () => {
+    it("should return the cleaned string with txt()", () => {
+      let dsclnstr = new dropsuit_clnstr(input, false);
+      let clnstr_txt_output = dsclnstr.clnstr(null).txt();
+      console.log("\n\nOutput clnstr().txt():", [clnstr_txt_output], "\n\n");
+      assert.equal(clnstr_txt_output, expected_output_clnstr_txt);
+    });
+
+    it("should return the cleaned string with nmb()", () => {
+      let dsclnstr = new dropsuit_clnstr(input, false);
+      let clnstr_nmb_output = dsclnstr.clnstr(null).nmb();
+      console.log("\n\nOutput clnstr().nmb():", [clnstr_nmb_output], "\n\n");
+      assert.equal(clnstr_nmb_output, expected_output_clnstr_nmb);
+    });
+
+    it("should return the cleaned string with pnc()", () => {
+      let dsclnstr = new dropsuit_clnstr(input, false);
+      let clnstr_pnc_output = dsclnstr.clnstr(null).pnc();
+      console.log("\n\nOutput clnstr().pnc():", [clnstr_pnc_output], "\n\n");
+      assert.equal(clnstr_pnc_output, expected_output_clnstr_pnc);
+    });
+  });
+});
+
+//#endregion
